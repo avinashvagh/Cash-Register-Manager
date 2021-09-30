bc55c0dc8f2d9d80b06abb827aa53bf43870fad1
Avinash Vagh <78526868+avinashvagh@users.noreply.github.com>
2021-09-30 14:12:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ checkButton.addEventListener ("click", function validateBillAndCashAmount() {
     hideMessage();
     if (billAmount.value > 0) {
 
-        if(cashGiven.value>= billAmount.value){
+        if(cashGiven.value >= billAmount.value){
 
             const amountToBeReturned = cashGiven.value - billAmount.value;
 
