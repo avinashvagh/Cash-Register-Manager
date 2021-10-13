8187f168c28b0c4a6f98d83556a68f15304ecbdf
Avinash Vagh <78526868+avinashvagh@users.noreply.github.com>
2021-10-13 12:34:08 +0530

    undo change

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ checkButton.addEventListener ("click", function validateBillAndCashAmount() {
         if(cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
-        } else if {
+        } else {
             showMessage("Do you wanna wash plates?");
         }
     } else {
