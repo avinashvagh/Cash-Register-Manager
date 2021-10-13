e886c0e253783522c74df8276c64df1a59b6ffcd
Avinash Vagh <78526868+avinashvagh@users.noreply.github.com>
2021-10-13 12:25:22 +0530

    else if

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ checkButton.addEventListener ("click", function validateBillAndCashAmount() {
         if(cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
-        } else {
+        } else if {
             showMessage("Do you wanna wash plates?");
         }
     } else {
